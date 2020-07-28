8946187dde3df454389e0389ff4375fb0cdca36d
liebercanis <mgold@unm.edu>
2020-07-28 12:13:19 -0600

    fix path

--- rootlogon.C
+++ rootlogon.C
@@ -2,8 +2,8 @@
   printf("\n this is rootlogon for baconSim \n");
   TString arch=gSystem->GetBuildArch();
   gROOT->LoadMacro("util.C");
-  cout << " arch is " << arch << endl; 
-  iload = gSystem->Load("$BACONSIM/io/obj/libBaconSimRoot.so");
+  cout << " arch is " << arch << endl;
+  iload = gSystem->Load("$BACONSIM/lib/libBaconSimRoot.so");
   printf(" loaded libBaconSimRoot.so = %i zero is success! \n",iload);
 }
 
